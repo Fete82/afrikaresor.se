73032bb59d4c2088ee91b2549ff0656e8ad9d460
Edvin Lindahl <fete.edvin@gmail.com>
2022-06-23 14:52:57 +0200

    return of the single, for regular posts (removed to see if template worked correctly)

---
+++ wp-content/themes/golemtheme/single.php
@@ -0,0 +1,38 @@
+<?php
+get_header();
+?>
+<div class="single-container">
+    <?php
+    if (have_posts()) {
+        while (have_posts()) {
+            the_post(); ?>
+
+
+            <div class="single-image">
+                <?php
+                the_post_thumbnail('large'); ?>
+
+            </div>
+            <div>
+                <h1 class="display-3 font-weight-bold bg-success text-light p-2"><?php echo the_title(); ?></h1>
+            </div>
+            <div>
+                <div class="single-content">
+                    <p class="display-5">
+                        <?php the_content(); ?>
+                    </p>
+                    <div>
+                        <button type="button" class="my-5 btn btn-success btn-lg">Boka resa nu</button>
+                    </div>
+                    <div class="comments-field" id="comments">
+                            <?php comments_template('template-parts/comments.php'); ?>
+                    </div>
+                </div>
+            </div>
+
+</div>
+
+<?php     }
+    }
+    get_footer();
+?>
